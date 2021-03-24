b21f6c18523db81967f5952a92896d2ed9563e11
ji sun lee <ellijs@hotmail.com>
2021-03-24 18:45:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function addToCart(item) {
   //let itemName = "name";
   let newItem = {itemName: item, itemPrice: price}
   cart.push(newItem);
-  console.log(`${cart.newItem['itemName']} has been added to your cart.`)
+  return`${newItem['itemName']} has been added to your cart.`
 }
 
 function viewCart() {
