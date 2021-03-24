496f7ab2f770efc3a9e86011c36227e4d564d67b
ji sun lee <ellijs@hotmail.com>
2021-03-24 19:52:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 //      let total = cart.reduce((accumulator, currentValue) => accumulator + currentValue.itemPrice, initialValue)
 //      return "Your total is \$ ${total}.";
   let total = cart.reduce((accumulator, currentValue) => {return accumulator + currentValue.itemPrice}, 0);
-  return total;
+  return "Your total is \$ ${total}.";
 }
 
 function removeFromCart(item) {
