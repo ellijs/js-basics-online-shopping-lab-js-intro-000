bef646baf7728ca3ea126f3495aad0cb905aec76
ji sun lee <ellijs@hotmail.com>
2021-03-24 19:13:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,11 @@ function viewCart() {
   } else {
       let string = "In your cart, you have "
       for (let i = 0; i < cart.length; i++) {
-      string += `${i === cart.length -1 ? "and " : ""}${cart[i].itemName} at \$${cart[i].itemPrice}${i === cart.length -1 ? "." : ", "}`
+        if (cart.length === 1) {
+          string += `${cart[i].itemName} at \$${cart[i].itemPrice}${i === cart.length -1 ? "." : ", "}`
+        } else {
+          string += `${i === cart.length -1 ? "and " : ""}${cart[i].itemName} at \$${cart[i].itemPrice}${i === cart.length -1 ? "." : ", "}`
+        }
       }
       return string;
     }
